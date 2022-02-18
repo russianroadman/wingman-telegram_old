650dc361f670e5c6bcd46f9b2878eb208ae39ab3
russianroadman <realgar2@gmail.com>
2022-02-18 22:19:11 +0800

    tests

---
+++ src/test/java/com/tanto/wingman/Dummy.java
@@ -0,0 +1,23 @@
+package com.tanto.wingman;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Dummy {
+
+    @Test
+    public void a(){
+        Assertions.assertEquals(1, 1);
+    }
+
+    @Test
+    public void b(){
+        Assertions.assertEquals(1, 2);
+    }
+
+    @Test
+    public void c(){
+        Assertions.assertEquals(1, 3);
+    }
+
+}
